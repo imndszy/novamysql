022255138001814ee999800f33b8599e4b95e146
szy <shizhenyu96@gmail.com>
2016-10-06 23:34:42 +0800

    first init

---
+++ testdb.py
@@ -0,0 +1,25 @@
+# -*- coding:utf8 -*-
+# Author: shizhenyu96@gamil.com
+# github: https://github.com/imndszy
+import unittest
+import logging
+
+from db import (create_engine,select,update,insert)
+
+class DbTestCase(unittest.TestCase):
+    def setUp(self):
+        logging.basicConfig(level=logging.DEBUG)
+        create_engine('szy', '123456', 'weixin', 'localhost', charset='utf8')
+
+    def tearDown(self):
+        pass
+
+    def test_select(self):
+        pass
+
+    def test_update(self):
+        pass
+
+    def test_insert(self):
+        self.assertEqual(insert('test',number=21,name='ads'),1,'test insert fail')
+
